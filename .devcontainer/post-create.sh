ca6d2d2f54037646f89bc4b2d988f4920e94633c
Joe Bethke <a9q25zz@mmm.com>
2020-09-03 11:59:21 -0500

    trying to log those creds

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,6 +2,8 @@ echo "Install azds cli..."
 ./.devcontainer/install-azds.sh > /dev/null
 echo
 echo "Azure Login with local environment vars"
+echo $AzureUsername
+echo $AzurePassword
 az login --username $AzureUsername --password $AzurePassword
 echo
 echo "Setup az aks credentials"
@@ -12,3 +14,4 @@ azds space select --name $(az account show --query "user.name" --output tsv | se
 echo
 echo "Set AppConfigurationConnectionString in dotnet user-secrets store"
 dotnet user-secrets set --project "src/services/common/Services/Services.csproj" AppConfigurationConnectionString $(az appconfig credential list --name $AzureAppConfigName --resource-group $AzureResourceGroupName --query "[?name=='Primary'].connectionString | [0]" --output tsv)
+export AzurePassword=""
