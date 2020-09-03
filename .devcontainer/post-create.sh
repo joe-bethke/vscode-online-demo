6832f6b15a5c42bcc8e9df55df06283dd9ecfac7
Joe Bethke <a9q25zz@mmm.com>
2020-09-03 11:43:44 -0500

    adding more readable logs

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,14 @@
-./.devcontainer/install-azds.sh
+echo "Install azds cli..."
+./.devcontainer/install-azds.sh > /dev/null
+echo
+echo "Azure Login with local environment vars"
 az login -u $1 -p $2
+echo
+echo "Setup az aks credentials"
 az aks get-credentials --name $AzureAksName --resource-group $AzureResourceGroupName
+echo
+echo "Create a new dev space"
 azds space select --name $(az account show --query "user.name" --output tsv | sed "s/\@.*//")
-dotnet user-secrets set --project "../src/services/common/Services/Services.csproj" AppConfigurationConnectionString $(az appconfig credential list --name $AzureAppConfigName --resource-group $AzureResourceGroupName --query "[?name=='Primary'].connectionString | [0]" --output tsv)
+echo
+echo "Set AppConfigurationConnectionString in dotnet user-secrets store"
+dotnet user-secrets set --project "src/services/common/Services/Services.csproj" AppConfigurationConnectionString $(az appconfig credential list --name $AzureAppConfigName --resource-group $AzureResourceGroupName --query "[?name=='Primary'].connectionString | [0]" --output tsv)
