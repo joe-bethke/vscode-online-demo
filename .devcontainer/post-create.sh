498d2a8afddc70d9ef8c44e510558f6a8635d175
Joe Bethke <a9q25zz@mmm.com>
2020-09-03 11:30:15 -0500

    trying to get logs for other post commands

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,1 +1,5 @@
 ./.devcontainer/install-azds.sh
+az login
+az aks get-credentials --name $AzureAksName --resource-group $AzureResourceGroupName
+azds space select --name $(az account show --query "user.name" --output tsv | sed "s/\@.*//")
+dotnet user-secrets set --project "../src/services/common/Services/Services.csproj" AppConfigurationConnectionString $(az appconfig credential list --name $AzureAppConfigName --resource-group $AzureResourceGroupName --query "[?name=='Primary'].connectionString | [0]" --output tsv)
